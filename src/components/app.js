f90954f145b2b855669a1a0d10e537c2d3f1ff47
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-06-12 16:06:23 -0400

    actualizacion de app.js

--- src/components/app.js
+++ src/components/app.js
@@ -75,6 +75,16 @@ export const App = () => {
 
     }
 
+    const asda = (cantidad) => {
+        console.log(cantidad)
+        switch (true) {
+            case (cantidad > 0 && cantidad <= 2): return <span className="ml-3 text-info font-weight-bold">Queda poco!</span>;
+            case (cantidad > 2 && cantidad <= 5): return <span className="ml-3 text-warning font-weight-bold">Trabaje trabaje!</span>;;
+            case cantidad > 5: return <span className="ml-3 text-danger font-weight-bold">pff, flojo</span>;
+        }
+
+    }
+
     return (<>
         <div className="container bg-secondary rounded">
             <div className="row mt-5 ">
@@ -94,18 +104,24 @@ export const App = () => {
                                 {
                                     state.tareas.length > 0 ? (
 
-                                    state.tareas.map((ele, i) => {
-                                        return <li key={i} className="list-group-item" >{ele.tarea}<i id="eliminar" className="fas fa-times" onClick={e => clickEliminar(e, i)}></i></li>
-                                    })
-                                    ):
-                                    (
-                                        <li className="list-group-item bg-success"><span className="mr-4">No hay Tareas pendientes.</span><i className="fas fa-check fa-2x"></i><i className="fas fa-check fa-2x"></i><i className="fas fa-check fa-2x"></i></li>
-                                    )
+                                        state.tareas.map((ele, i) => {
+                                            return <li key={i} className="list-group-item" >{ele.tarea}<i id="eliminar" className="fas fa-times" onClick={e => clickEliminar(e, i)}></i></li>
+                                        })
+                                    ) :
+                                        (
+                                            <li className="list-group-item bg-success"><span className="mr-4">No hay Tareas pendientes.</span><i className="fas fa-check fa-2x"></i><i className="fas fa-check fa-2x"></i><i className="fas fa-check fa-2x"></i></li>
+                                        )
                                 }
                             </ul>
                         </div>
                         <div className="card-footer text-muted text-left">
                             {state.tareas.length} item restantes.
+
+                            {
+                                asda(state.tareas.length)
+                            }
+
+
                         </div>
                     </div>
                 </div>
